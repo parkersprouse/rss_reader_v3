d885db4a5799bc85639bf10797ea7210470da0c9
Parker Sprouse <parker@parkersprouse.me>
2025-02-14 01:19:31 -0600

    Time to get spicy by restarting using bun and web components instead of nuxt and vue

--- bin/ensure_bun.js
+++ bin/ensure_bun.js
@@ -1,7 +1,7 @@
-if (!/pnpm\.(?:c|m)*js$|pnpmpkg$/.test(process.env.npm_execpath)) {
+if (!process.env._.includes('bun')) {
   const red = (str) => `\u001B[1;31m${str}\u001B[0;0m`;
   console.error(red('*'.repeat(45)));
-  console.error(red('* You must use pnpm as your package manager *'));
+  console.error(red('* You must use bun as your package manager *'));
   console.error(red('*'.repeat(45)), '\n');
   process.exit(1);
 }
--- modules.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@eslint-community/eslint-plugin-eslint-comments/configs';
-declare module '@eslint/eslintrc';
-declare module '@eslint/js';
-declare module 'eslint-plugin-unicorn';
-declare module 'eslint-plugin-vue';
